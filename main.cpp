52f973682205c09abb769fa671a6fa05cb91981b
scurila <stefania_curila@yahoo.ro>
2022-03-25 23:31:06 +0100

    Deleted comments

--- main.cpp
+++ main.cpp
@@ -3,18 +3,12 @@
 #include "automate.h"
 
 int main(void) {
-   string chaine("(((2+5))*5)");
+   string chaine("((2+5)*5+8)*2+7*3+2");
    cout << "Operation: " << chaine << endl;
+   
    Lexer l(chaine);
-
-  // Symbole * s;
-   //while(*(s=l.Consulter())!=FIN) {
-     // s->Affiche();
-     // cout<<endl;
-     // l.Avancer();
-     Automate automate (chaine);
-     automate.run();
-  // }
+   Automate automate (chaine);
+   automate.run();
    return 0;
 }
 
